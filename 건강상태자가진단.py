b1cd7a5c12d6713e373075d7a052a6fed7e4d767
ovclid <53159064+ovclid@users.noreply.github.com>
2022-03-11 18:07:30 +0900

    Update 건강상태자가진단.py

--- 건강상태자가진단.py
+++ 건강상태자가진단.py
@@ -47,183 +47,201 @@ def save_html (driver, filename):
     
                            
     
-if __name__ ==  "__main__" :
-    
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    print("try to login...")
-    
-    driver.get('https://hcs.eduro.go.kr/#/loginHome')
+#if __name__ ==  "__main__" :
+
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome("C:\chromedriver\chromedriver.exe")
+
+print("try to login...")
+
+driver.get('https://hcs.eduro.go.kr/#/loginHome')
+time.sleep(1)
+
+
+# 로그인 버튼을 찾고 클릭합니다
+
+btn = driver.find_element_by_id('btnConfirm2')
+btn.click()
+time.sleep(3)
+
+
+btn =  driver.find_element_by_id('schul_name_input')
+btn.click()
+time.sleep(1)
+
+obj1 = Select(driver.find_element_by_id("sidolabel"))
+obj1.select_by_index(8)
+time.sleep(1)
+
+obj1 = Select(driver.find_element_by_id("crseScCode"))
+obj1.select_by_index(2)
+btn =  driver.find_element_by_id('orgname')
+btn.send_keys("늘봄초")
+
+xpath = '//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[3]/td[2]/button'
+btn =  driver.find_element_by_xpath(xpath)
+btn.click()
+time.sleep(1)
+
+
+xpath = '//*[@id="softBoardListLayer"]/div[2]/div[1]/ul/li/a/p/a'
+btn =  driver.find_element_by_xpath(xpath)
+btn.click()
+time.sleep(1)
+
+xpath = '//*[@id="softBoardListLayer"]/div[2]/div[2]/input'
+btn =  driver.find_element_by_xpath(xpath)
+btn.click()
+
+btn =  driver.find_element_by_id('user_name_input')
+btn.send_keys("하지훈")
+
+btn =  driver.find_element_by_id('birthday_input')
+btn.send_keys("111117")
+btn =  driver.find_element_by_id('btnConfirm')
+btn.click()
+time.sleep(2)
+
+#xpath = '//*[@id="WriteInfoForm"]/table/tbody/tr/td/input'
+#btn =  driver.find_element_by_xpath(xpath)
+#btn.send_keys('8080')
+
+class_key_img = driver.find_element_by_class_name("keyboard-img")
+class_key_img.click()
+time.sleep(2)
+
+num8 = "#password_mainDiv > div:nth-child(8) > a:nth-child(4)"
+num8_ele = driver.find_element_by_css_selector(num8)
+
+num0 = "#password_mainDiv > div:nth-child(4) > a"
+num0_ele = driver.find_element_by_css_selector(num0)
+
+num_list = [num8_ele, num0_ele, num8_ele, num0_ele]
+for num in num_list:
+    num.click()
     time.sleep(1)
 
+#btn =  driver.find_element_by_id('tk_enter_r') 
+#btn.click()
 
-    # 로그인 버튼을 찾고 클릭합니다
+btn =  driver.find_element_by_id('btnConfirm')
+btn.click()
+
+
+"""
+
+print("td, pw 입력할 곳을 찾습니다...")
+# id, pw 입력할 곳을 찾습니다.
+tag_id = driver.find_element_by_name('id')
+tag_pw = driver.find_element_by_name('pw')
+tag_id.clear()
+time.sleep(1)
+
+# id 입력
+tag_id.click()
+pyperclip.copy('cbmss1357')
+tag_id.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
+
+# pw 입력
+tag_pw.click()
+pyperclip.copy('cbmss1357!')
+tag_pw.send_keys(Keys.CONTROL, 'v')
+time.sleep(1)
+
+# 로그인 버튼을 클릭합니다
+login_btn = driver.find_element_by_id('log.login')
+login_btn.click()
+
+time.sleep(1)
+
+
+driver.get('https://nid.naver.com/nidlogin.login')
+driver.find_element_by_name('id').send_keys('ovclid')
+driver.find_element_by_name('pw').send_keys('*sang716901')
+driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+
+
+driver = log_in(driver)
+
+soup_html = bs(driver.page_source, 'html.parser')
+
+subs = soup_html.findAll(class_="p_photo_d")
+
+for i in range(5):  #len(subs)
     
-    btn = driver.find_element_by_id('btnConfirm2')
-    btn.click()
-    time.sleep(3)
+    sub_url = subs[i].find("a")["href"]
+    driver.get(sub_url)
 
+    driver.find_element_by_class_name('_spi_blog').click()  # spi_btn_blog
+    time.sleep(0.5)
 
-    btn =  driver.find_element_by_id('schul_name_input')
-    btn.click()
-    time.sleep(1)
-
-    obj1 = Select(driver.find_element_by_id("sidolabel"))
-    obj1.select_by_index(8)
-    time.sleep(1)
-
-    obj1 = Select(driver.find_element_by_id("crseScCode"))
-    obj1.select_by_index(2)
-    btn =  driver.find_element_by_id('orgname')
-    btn.send_keys("늘봄초")
-
-    xpath = '//*[@id="softBoardListLayer"]/div[2]/div[1]/table/tbody/tr[3]/td[2]/button'
-    btn =  driver.find_element_by_xpath(xpath)
-    btn.click()
-    time.sleep(1)
-
-
-    xpath = '//*[@id="softBoardListLayer"]/div[2]/div[1]/ul/li/a/p/a'
-    btn =  driver.find_element_by_xpath(xpath)
-    btn.click()
-    time.sleep(1)
+    handles = driver.window_handles
+    driver.switch_to.window(handles[1])
     
-    xpath = '//*[@id="softBoardListLayer"]/div[2]/div[2]/input'
-    btn =  driver.find_element_by_xpath(xpath)
-    btn.click()
-
-    btn =  driver.find_element_by_id('user_name_input')
-    btn.send_keys("하지훈")
-
-    btn =  driver.find_element_by_id('birthday_input')
-    btn.send_keys("111117")
-    btn =  driver.find_element_by_id('btnConfirm')
-    btn.click()
-    time.sleep(2)
+    driver.find_element_by_id('_submit').click()  # spi_btn_blog
+    time.sleep(0.5)
+    driver.switch_to.window(handles[0])
+        
+    #driver.find_element_by_class_name('pop_btn')
     
-    xpath = '//*[@id="WriteInfoForm"]/table/tbody/tr/td/input'
-    btn =  driver.find_element_by_xpath(xpath)
-    btn.send_keys('8080')
-    
-    btn =  driver.find_element_by_id('btnConfirm')
-    btn.click()
-    
-
-    
-    
-    """
-    
-    print("td, pw 입력할 곳을 찾습니다...")
-    # id, pw 입력할 곳을 찾습니다.
-    tag_id = driver.find_element_by_name('id')
-    tag_pw = driver.find_element_by_name('pw')
-    tag_id.clear()
-    time.sleep(1)
-
-    # id 입력
-    tag_id.click()
-    pyperclip.copy('cbmss1357')
-    tag_id.send_keys(Keys.CONTROL, 'v')
-    time.sleep(1)
-
-    # pw 입력
-    tag_pw.click()
-    pyperclip.copy('cbmss1357!')
-    tag_pw.send_keys(Keys.CONTROL, 'v')
-    time.sleep(1)
-
-    # 로그인 버튼을 클릭합니다
-    login_btn = driver.find_element_by_id('log.login')
-    login_btn.click()
-
-    time.sleep(1)
+save_html(driver, f"blog.html")
     
 
-    driver.get('https://nid.naver.com/nidlogin.login')
-    driver.find_element_by_name('id').send_keys('ovclid')
-    driver.find_element_by_name('pw').send_keys('*sang716901')
-    driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
- 
+try:
+    handles = driver.window_handles
+    driver.switch_to.window(handles[0])
+
+    search = '//*[@id="contents"]/div[2]/section/div[2]/div[3]/span/a/img'
+    driver.find_element_by_xpath(search).click()
+    driver.implicitly_wait(5)
+
+    handles = driver.window_handles
+    bizInfoListPopupHandleIndex= len(handles) - 1 # PopUP 창이 뜨지 않을 경우 1로 변경해야
+    driver.switch_to.window(handles[bizInfoListPopupHandleIndex])
     
-    driver = log_in(driver)
+    result_cominfo = '//*[@id="mySheet"]/tbody/tr[3]/td/div/div[1]/table/tbody/tr[2]/td[5]'
+    v
+    driver.find_element_by_xpath(result_cominfo).click()            
+    
+
+
+    #first_filename = input("!!!! 1차 파업 저장 파일 이름: ")
+    #save_html(driver, f"{first_filename}.html")
 
     soup_html = bs(driver.page_source, 'html.parser')
 
-    subs = soup_html.findAll(class_="p_photo_d")
-    
-    for i in range(5):  #len(subs)
-        
-        sub_url = subs[i].find("a")["href"]
-        driver.get(sub_url)
-
-        driver.find_element_by_class_name('_spi_blog').click()  # spi_btn_blog
-        time.sleep(0.5)
-
-        handles = driver.window_handles
-        driver.switch_to.window(handles[1])
-        
-        driver.find_element_by_id('_submit').click()  # spi_btn_blog
-        time.sleep(0.5)
-        driver.switch_to.window(handles[0])
-         
-        #driver.find_element_by_class_name('pop_btn')
-        
     save_html(driver, f"blog.html")
-        
-
-    try:
-        handles = driver.window_handles
-        driver.switch_to.window(handles[0])
-
-        search = '//*[@id="contents"]/div[2]/section/div[2]/div[3]/span/a/img'
-        driver.find_element_by_xpath(search).click()
-        driver.implicitly_wait(5)
-
-        handles = driver.window_handles
-        bizInfoListPopupHandleIndex= len(handles) - 1 # PopUP 창이 뜨지 않을 경우 1로 변경해야
-        driver.switch_to.window(handles[bizInfoListPopupHandleIndex])
-        
-        result_cominfo = '//*[@id="mySheet"]/tbody/tr[3]/td/div/div[1]/table/tbody/tr[2]/td[5]'
-        v
-        driver.find_element_by_xpath(result_cominfo).click()            
-        
-
-
-        #first_filename = input("!!!! 1차 파업 저장 파일 이름: ")
-        #save_html(driver, f"{first_filename}.html")
     
-        soup_html = bs(driver.page_source, 'html.parser')
-
-        save_html(driver, f"blog.html")
-        
-        
-        handles = driver.window_handles
-        driver.switch_to.window(handles[bizInfoListPopupHandleIndex]) 
-                                                   
-        src = driver.find_element_by_name("hidden_frame").get_attribute("src")
-
-        
-        driver.get(src)         
-        soup_html = bs(driver.page_source, 'html.parser')
-
-    except TimeoutException:
-        print("Timeout")
-        continue
     
-    except NoSuchElementException:
-        print("No duplicate order detected")
-        continue
+    handles = driver.window_handles
+    driver.switch_to.window(handles[bizInfoListPopupHandleIndex]) 
+                                                
+    src = driver.find_element_by_name("hidden_frame").get_attribute("src")
+
     
-    except NoAlertPresentException:
-        print("No Alert Present")
-        continue
+    driver.get(src)         
+    soup_html = bs(driver.page_source, 'html.parser')
+
+except TimeoutException:
+    print("Timeout")
+    continue
+
+except NoSuchElementException:
+    print("No duplicate order detected")
+    continue
+
+except NoAlertPresentException:
+    print("No Alert Present")
+    continue
+
+except AttributeError:
+    print("AttributeError Present")
+    continue
     
-    except AttributeError:
-        print("AttributeError Present")
-        continue
-     
-    except UnexpectedAlertPresentException:
-        alert = driver.switch_to_alert()
-        alert.accept()
-        print("Unexpected alert, yo!") 
-        continue
-    """
+except UnexpectedAlertPresentException:
+    alert = driver.switch_to_alert()
+    alert.accept()
+    print("Unexpected alert, yo!") 
+    continue
+"""
