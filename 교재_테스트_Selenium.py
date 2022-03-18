e4d242fb2922c0a42cf6fbbcd51d94129d053c31
ovclid <53159064+ovclid@users.noreply.github.com>
2022-03-18 09:25:48 +0900

    Add files via upload

---
+++ 교재_테스트_Selenium.py
@@ -0,0 +1,47 @@
+# %%
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+import time
+
+print(1)
+# webdriver 옵션 설정 : 화면 최대사이즈, 자동제어 메시지 나타나지 않게 설정
+opt = Options()
+opt.add_argument('--start-maximized')
+opt.add_experimental_option("excludeSwitches", ["enable-automation"])
+opt.add_experimental_option('useAutomationExtension', False)
+
+#%%
+
+# webdriver를 통한 크롬 구동
+driver = webdriver.Chrome("C:\chromedriver\chromedriver.exe", options=opt)
+time.sleep(3)
+driver.get('http://sbasu.pythonanywhere.com/tastyFoodApp/')
+
+print("get home pages")
+
+ele = driver.find_element_by_link_text("Create New Profile")
+ele.click()
+time.sleep(3)
+driver.back()
+
+#%%
+ele = driver.find_element_by_partial_link_text("New")
+ele.click()
+time.sleep(3)
+driver.back()
+
+ele = driver.find_element_by_css_selector("a[href='/tastyFoodApp/create']")
+ele.click()
+time.sleep(3)
+driver.back()
+
+ele = driver.find_element_by_xpath('//*[@id="homePageLinks"]/ul/li/a')
+ele.click()
+time.sleep(3)
+driver.back()
+
+ele = driver.find_element_by_xpath('/html/body/div/div/ul/li/a')
+ele.click()
+time.sleep(3)
+driver.back()
