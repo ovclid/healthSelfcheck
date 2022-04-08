a798567939169435b8a42f6b92982ea342c019ba
ovclid <53159064+ovclid@users.noreply.github.com>
2022-04-08 17:56:20 +0900

    Add files via upload

---
+++ 이미지테스트.py
@@ -0,0 +1,9 @@
+#%%
+#from urllib import request
+import urllib
+
+url = "https://rl6cz18qh.toastcdn.net/eduro/1.9.8/images/common/loginhome_group_institute.png"
+
+urllib.request.urlretrieve(url, "a.png")
+
+#%%
